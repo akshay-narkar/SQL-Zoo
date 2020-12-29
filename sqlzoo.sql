c63323fb5317073a157e94a185699f4f47494ff7
akshay-narkar <akshaynarkar1295@gmail.com>
2020-12-29 17:27:08 +0530

    Add SQL Joins Queries

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -238,4 +238,98 @@ where continent = 'Africa';
 
 --4 
 select count(name) from world 
-where area >= 1000000 
+where area >= 1000000 ;
+
+--5
+select sum(population) from world 
+where name IN  ('Estonia', 'Latvia', 'Lithuania') 
+
+--6
+select continent, count(*) from world
+group by continent;
+
+--7
+select continent, count(name) from world
+where population >= 10000000
+group by continent;
+
+--8
+select continent from world
+group by continent
+having SUM(population)>100000000;
+
+--JOIN and euros
+--1
+
+SELECT matchid,player FROM goal 
+  WHERE teamid='GER'
+
+  --2
+  SELECT id,stadium,team1,team2
+  FROM game
+where id = 1012;
+
+--3
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=matchid)
+where teamid='GER';
+
+--4
+select team1,team2,player from game
+join goal on (id=matchid)
+where player like 'Mario%';
+
+--5
+SELECT player, teamid, coach,gtime
+  FROM goal
+join eteam on teamid=id 
+ WHERE gtime<=10;
+
+ --6
+ select mdate,teamname from game
+join eteam on (team1=eteam.id)
+where coach = 'Fernando Santos';
+
+--7
+select player from goal 
+join game on id=matchid
+where stadium = 'National Stadium, Warsaw';
+
+--8
+SELECT distinct player
+  FROM goal JOIN game ON matchid = id 
+   where teamid <> 'GER'AND (team1 = 'GER' or team2 = 'GER');
+
+--9
+SELECT teamname, count(*) as Goals
+  FROM eteam join goal on teamid=id
+ group BY teamname;
+
+ --10
+ select stadium,count(*)
+from game join goal on matchid=id
+group by stadium
+
+--11
+
+SELECT matchid,mdate, count(matchid)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+group by matchid,mdate
+
+--12
+
+select matchid,mdate,count(matchid)
+from goal join game on id=matchid
+where teamid='GER'
+group by matchid,mdate;
+
+--13
+
+select mdate,team1, 
+SUM(case when teamid = team1 then 1 else 0 END) score1,
+team2,
+SUM(case when teamid = team2 then 1 else 0 END) score2
+from game left join goal on id=matchid
+group by mdate,team1,team2
+order by mdate, matchid,team1,team2;
